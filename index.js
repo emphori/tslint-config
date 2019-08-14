2879fad8bddaff4fbb88c5f9247af90488104315
Iain Reid <iainreid382@gmail.com>
2019-08-14 20:50:29 +0000

    Use TSLint recommended

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-  extends: 'tslint:latest',
+  extends: 'tslint:recommended',
   rules: {
     'interface-name': [true, 'never-prefix'],
     'max-line-length': [true, 140],
